bbc628b3c05ba62d75f1b8717c380ab9a3411064
Almircesar8 <cesar25silvafilho@gmail.com>
2024-10-31 02:27:53 -0300

    func_pesquisa_dados

--- Site/dados.js
+++ Site/dados.js
@@ -1,18 +1,22 @@
 let dados = [
     {
-        nome: "André Ramalho",
-        descricao: "André Ramalho é um zagueiro experiente que chegou ao Corinthians com o status de reforço de peso. Com passagens por grandes clubes europeus, o defensor trouxe para o Timão sua solidez defensiva, liderança e experiência em grandes jogos. Sua chegada fortaleceu a zaga corinthiana, proporcionando mais segurança e organização à equipe. Com sua liderança natural, André Ramalho rapidamente se tornou um dos pilares do time e conquistou a confiança da Fiel Torcida.",
-        insta: "https://www.instagram.com/ramalho92/",
-        wiki: "https://pt.wikipedia.org/wiki/André_Ramalho",
-        nacionalidade: "Brasileiro",
-        idade: 32,
-        camisa: 5,
-        posicao: "zagueiro",
-        jogos: 13,
-        titulos: 0,
-        gols: 1,
-        assistencias: 0,
-        tags: "andre",
-        foto: "andre_ramalho.png",
+        nome: "História",
+        descricao: "O jogo segue a trajetória do personagem principal enquanto ele avança por diferentes fases repletas de desafios, enfrentando inimigos e obstáculos que exigem habilidade e estratégia. O objetivo é derrotar o grande vilão, o 'boss', que se encontra no final de seu caminho, mas o percurso é repleto de perigos e reviravoltas, que vão testar a habilidade e a perseverança do jogador.<br> A história se passa em um futuro distante, no ano de 2708, onde o mundo é amplamente conectado por uma gigantesca rede digital chamada Rede Gan. Nesse cenário hiperconectado, a tecnologia faz parte de todos os aspectos da vida, e a sociedade depende dela para suas funções mais básicas. Contudo, um vírus maligno emerge na rede, com o objetivo de controlar e dominar todos os sistemas digitais, ameaçando o funcionamento do mundo como um todo. <br> O protagonista, conhecido como 'Error 504', é uma espécie de herói digital programado para enfrentar essa ameaça. Ele é uma última linha de defesa contra o vírus, navegando por sistemas corrompidos elutando contra adversários digitais que tentam bloquear seu caminho. À medida que ele avança, ojogador descobre mais sobre a origem do vírus e a missão de Error 504, que não é apenas de defesa, mas de salvar o mundo da completa dominação tecnológica. A jornada do personagem se torna umabatalha épica pela liberdade e segurança do mundo, mantendo o jogador envolvido e desafiado a cada nova fase.",
+        tags: "historia, história, lore, jogo",
+        foto: "background.jpeg",
+    },
+
+    {
+        nome: "Desenvolvimento",
+        descricao: "Todo o desenvolvimento do jogo foi realizado em GD Script, a linguagem de programação nativa da plataforma Godot, escolhida por sua integração e eficiência para criação de jogos interativos. GD Script permitiu a implementação das mecânicas do jogo, controle de personagens, física e progressão de fases de forma otimizada e acessível, maximizando o potencial da engine. Além disso, toda a parte gráfica, incluindo sprites, fundos, animações e efeitos visuais, foi cuidadosamente elaborada no Pikel, garantindo uma estética coesa e imersiva. O uso do Pikel contribuiu para criar visuais que remetem ao estilo retrô, dando ao jogo uma atmosfera nostálgica e, ao mesmo tempo, envolvente para os jogadores. Essa combinação entre GD Script e Pikel proporcionou um equilíbrio entre mecânicas fluidas e visuais atraentes, fundamentais para o sucesso do projeto.<br>Na pesquisa estudamos a documentação do godot e do Pikel, além de termos visto algumas vídeo-aulas sobres estes conteudos, como usar a linguagem ou como mexer na plataforma, mas também aprendemos uns com os outros, sempre nos auxiliando.",
+        tags: "processo, passo-a-passo",
+        foto: "logo_godot.png",
+    },
+
+    {
+        nome: "  ",
+        descricao: "  ",
+        tags: "  ",
+        foto: "  ",
     },
 ]
